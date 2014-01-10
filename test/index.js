7417e5d01c5543f58b013977de88047b6d483262
Qasim Zaidi <qasim@zaidi.me>
2014-01-10 11:20:00 +0530

    anydb - merge upstream changes

--- test/index.js
+++ test/index.js
@@ -2,36 +2,43 @@ var test = require('tap').test;
 
 var anydbsql = require('../anydb-sql');
 
+var grouper = require('../lib/grouper');
+
 var path = require('path');
 
+var util = require('util');
 
 var db = anydbsql({
   url: 'sqlite3://', 
   connections: 1
 });
 
+var post = db.define({
+    name: 'posts',
+    columns: {
+        'id': {primaryKey: true}, 
+        'title':{}, 
+        'content':{}, 
+        'userId': {}
+    },
+    has: {'user': {from: 'users'}}
+});
+
 var user = db.define({
   name: 'users',
-  columns: ['id', 'name']
+  columns: {
+      id: { primaryKey: true }, 
+      name:{}
+  },
+  has: {posts: {from: 'posts', many: true}}
 });
 
-
-test('normalizer', function(t) {
-    var n = anydbsql.normalizer;
-    t.test('normalizes well', function(t) {
-        var normalized = n({'a.b':1, 'a.c': 2, b: 3, 'c.a': 1, 'c.b': 2});
-        t.deepEquals(normalized, {
-            a: {b: 1, c: 2},
-            b: 3,
-            c: {a: 1, b: 2}
-        });
-        t.end();
-    });
-});
 
 test('anydb-sql', function(t) {
 
-  db.query('create table users (id integer primary key, name text);', function(err) {
+  db.query('create table users (id integer primary key, name);\n'
+          +'create table posts (id integer primary key, title, content, userId);', function(err) {
+    
     t.notOk(err, 'creating table failed: ' + err);
 
     t.test('insert exec', function(t) {
@@ -48,24 +55,40 @@ test('anydb-sql', function(t) {
       });
     });
 
-    t.test('insert transaction', function(t) {
-      db.begin(function(err, tx){
-        user.insert({id: 2, name: 'test2'}).execWithin(tx);
-        user.insert({id: 3, name: 'test3'}).execWithin(tx);
-        tx.commit(function(err) {
-          t.notOk(err, 'two inserts within transaction');
-          t.end();
-        });
-      });
+    t.test('where get .then', function(t) {
+        user.where({id: 1}).get().then(function(user) {
+            t.deepEquals(user, {id: 1, name: 'test'});
+            t.end();
+        }).done();
     });
 
-    t.test('incorrect query', function(t) {
-      user.insert({id: 4, name: 'lol'}).returning(user.id).exec(function(err, results) {
-        t.ok(err, 'incorrect query');
-        t.notOk(results, 'should return null results: ' + results)
-        t.end();
-      });
+    t.test('insert transaction', function(t) {
+        return db.transaction(function(tx) {
+            return user.insert({id: 2, name: 'test2'}).execWithin(tx)
+            .then(function() {
+                return user.insert({id: 3, name: 'test3'}).execWithin(tx);
+            }).then(function() {
+                return user.where({id: 2}).getWithin(tx)
+            }).then(function(res) {
+                t.ok(res, 'getWithin transaction should work');
+                t.end();
+                return true;
+            });
+        }).done(null, function(e) {
+            t.notOk(e, 'db.transaction should not throw');
+            t.end();
+        })
     });
+
+    t.test('failed transaction', function(t) {
+        return db.transaction(function(tx) {
+            throw new Error("Transaction failed");
+        }).done(null, function(e) {
+            t.ok(e, 'db.transaction should fail');
+            t.end();
+        })
+    });
+
 
     t.test('resultless get', function(t) {
       user.where({id: 40}).get(function(err, usr) {
@@ -81,37 +104,26 @@ test('anydb-sql', function(t) {
       var query = user.from(user.join(userx).on(user.id.equals(userx.id)))
         .where(userx.id.equals(1))
         .select(userx.id);
-      //console.log(query.toQuery())
 
       query.get(function(err, res) {
-        //console.log(err, res);
         t.equals(res.id,  1, 'user.id is 1');    
         t.end();
       });
     });
 
-    t.test('allof', function(t) {
-        var q = user.select(db.allOf(user, user));
-        var text = 'SELECT "users"."id" AS "users.id",'
-            + ' "users"."name" AS "users.name", "users"."id" AS "users.id",'
+    t.test('selectDeep', function(t) {
+        var q = user.from(user).selectDeep(user, user);
+        var text = 'SELECT "users"."id" AS "users.id##",'
+            + ' "users"."name" AS "users.name", "users"."id" AS "users.id##",'
             +' "users"."name" AS "users.name" FROM "users"';
         t.equals(q.toQuery().text, text);
         t.end();
     });
 
-    t.test('allObject', function(t) {
-        user.select(user.id, user.name).allObject("id", function(err, result) {
-            t.notOk(err, "has no error " + err);
-
-            user.select(user.id, user.name).exec(function(err, data) {
-                t.equals(Object.keys(result).length, data.length, "Amount of entries should match");
-
-                for (var i = 0; i < data.length; i++)
-                    t.equals(data[i].name, result[data[i].id], "Data should match");
-
-                t.end();
-            });
-        });
+    t.test('selectDeep treats aggregates properly', function(t) {
+        var alias = db.allOf(user.posts.id.count().as('postCount'))[0].alias;
+        t.equals(alias, 'users.postCount');
+        t.end();
     });
 
     t.test('db.close', function(t) {
